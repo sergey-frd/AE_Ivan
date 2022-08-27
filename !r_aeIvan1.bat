2bd7a943e0f046d3f761afe1b732cd3db0d68b7d
sergey-frd <sergey.frd@gmail.com>
2022-08-27 12:14:07 +0300

    220827
    220827

--- !r_aeIvan1.bat
+++ !r_aeIvan1.bat
@@ -1,5 +1,5 @@
 c:
 cd c:\Program Files\Adobe\Adobe After Effects 2022\Support Files\
-AfterFX   -noui -r  "e:\\Git\\P_h_o_t_o\\AE_JSX\\AE_Igor\\sf_Ivan__22_2.jsx"
+AfterFX   -noui -r  "e:\\Git\\P_h_o_t_o\\AE_JSX\\AE_Ivan\\sf_Ivan22_B1.jsx"
 
 
